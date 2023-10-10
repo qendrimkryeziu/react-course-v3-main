7167111abafbd0ded973b088da388eb729b7bcf2
Qendrim Kryeziu <qendrimkryeziu18@gmail.com>
2023-10-10 13:24:01 +0200

    mixmaster

--- 09-mixmaster/starter/src/App.jsx
+++ 09-mixmaster/starter/src/App.jsx
@@ -1,5 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
   About,
   HomeLayout,
@@ -12,6 +13,15 @@ import {
 
 import { loader as landingLoader } from "./pages/Landing";
 import { loader as singleCockailLoader } from "./pages/Cocktail";
+import { action as newsletterAction } from "./pages/Newsletter";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -23,17 +33,19 @@ const router = createBrowserRouter([
         index: true,
         element: <Landing />,
         errorElement: <SinglePageError />,
-        loader: landingLoader,
+        loader: landingLoader(queryClient),
       },
       {
         path: "cocktail/:id",
         errorElement: <SinglePageError />,
-        loader: singleCockailLoader,
+        loader: singleCockailLoader(queryClient),
         element: <Cocktail />,
       },
       {
         path: "newsletter",
         element: <Newsletter />,
+        action: newsletterAction,
+        errorElement: <SinglePageError />,
       },
       {
         path: "about",
@@ -54,6 +66,11 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 export default App;
--- 09-mixmaster/starter/src/pages/SinglePageError.jsx
+++ 09-mixmaster/starter/src/pages/SinglePageError.jsx
@@ -2,7 +2,8 @@ import { useRouteError } from "react-router-dom";
 
 const SinglePageError = () => {
   const error = useRouteError();
-  return <div>{error.message}</div>;
+  // return <h2>{error.message}</h2>;
+  return <h2>there was an error...</h2>;
 };
 
 export default SinglePageError;
